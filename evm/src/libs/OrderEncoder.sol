51ae23a7ae9adad5a1053d80199850f776ce22ba
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-03-19 17:42:07 +0100

    refactor(evm): updates OrderEncoder in order to be compatible with aztec 7683 implementation

--- evm/src/libs/OrderEncoder.sol
+++ evm/src/libs/OrderEncoder.sol
@@ -13,10 +13,13 @@ struct OrderData {
     uint32 destinationDomain;
     bytes32 destinationSettler;
     uint32 fillDeadline;
-    bytes data;
 }
+// bytes data;
 
 library OrderEncoder {
+    error InvalidOrderLength();
+    error OutOfRange();
+
     bytes constant ORDER_DATA_TYPE = abi.encodePacked(
         "OrderData(",
         "bytes32 sender,",
@@ -29,9 +32,9 @@ library OrderEncoder {
         "uint32 originDomain,",
         "uint32 destinationDomain,",
         "bytes32 destinationSettler,",
-        "uint32 fillDeadline,",
-        "bytes data)"
+        "uint32 fillDeadline)"
     );
+    //"bytes data)"
 
     bytes32 constant ORDER_DATA_TYPE_HASH = keccak256(ORDER_DATA_TYPE);
 
@@ -40,16 +43,62 @@ library OrderEncoder {
     }
 
     function id(OrderData memory order) internal pure returns (bytes32) {
-        return keccak256(encode(order));
+        return sha256(encode(order));
     }
 
     function encode(OrderData memory order) internal pure returns (bytes memory) {
-        return abi.encode(order);
+        return abi.encodePacked(
+            order.sender,
+            order.recipient,
+            order.inputToken,
+            order.outputToken,
+            order.amountIn,
+            order.amountOut,
+            order.senderNonce,
+            order.originDomain,
+            order.destinationDomain,
+            order.destinationSettler,
+            order.fillDeadline
+        );
     }
 
-    function decode(bytes memory orderBytes) internal pure returns (OrderData memory) {
-        (OrderData memory orderData) = abi.decode(orderBytes, (OrderData));
+    function decode(bytes memory orderBytes) internal pure returns (OrderData memory order) {
+        require(orderBytes.length == 268, InvalidOrderLength());
 
-        return orderData;
+        order.sender = bytesToBytes32(orderBytes, 0);
+        order.recipient = bytesToBytes32(orderBytes, 32);
+        order.inputToken = bytesToBytes32(orderBytes, 64);
+        order.outputToken = bytesToBytes32(orderBytes, 96);
+        order.amountIn = bytesToUint256(orderBytes, 128);
+        order.amountOut = bytesToUint256(orderBytes, 160);
+        order.senderNonce = bytesToUint256(orderBytes, 192);
+        order.originDomain = bytesToUint32(orderBytes, 224);
+        order.destinationDomain = bytesToUint32(orderBytes, 228);
+        order.destinationSettler = bytesToBytes32(orderBytes, 232);
+        order.fillDeadline = bytesToUint32(orderBytes, 264);
+
+        return order;
+    }
+
+    function bytesToBytes32(bytes memory data, uint256 offset) internal pure returns (bytes32 result) {
+        require(data.length >= offset + 32, OutOfRange());
+        assembly {
+            result := mload(add(data, add(32, offset)))
+        }
+    }
+
+    function bytesToUint256(bytes memory data, uint256 offset) internal pure returns (uint256 result) {
+        require(data.length >= offset + 32, OutOfRange());
+        assembly {
+            result := mload(add(data, add(32, offset)))
+        }
+    }
+
+    function bytesToUint32(bytes memory data, uint256 offset) internal pure returns (uint32 result) {
+        require(data.length >= offset + 4, OutOfRange());
+        assembly {
+            let word := mload(add(data, add(32, offset)))
+            result := shr(224, word) // shift down to get the top 4 bytes
+        }
     }
 }
