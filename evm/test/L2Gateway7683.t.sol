e92b00fcc7052518f8fc9673a2aab69386c4debc
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-07-08 14:49:50 +0200

    chore(evm): rn L2Gateway7683 test into L2Gateway7683Test

--- evm/test/L2Gateway7683.t.sol
+++ evm/test/L2Gateway7683.t.sol
@@ -4,4 +4,4 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 //import {L2Gateway7683} from "../src/L2Gateway7683.sol";
 
-contract L2Gateway7683 is Test {}
+contract L2Gateway7683Test is Test {}
